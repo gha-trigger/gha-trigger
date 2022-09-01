c968fff8f75c14aa58f1840c0064294cdd7a93fd
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-09-01 19:47:29 +0900

    fix: fix ci repo name

--- pkg/handler/lambda/slash_command.go
+++ pkg/handler/lambda/slash_command.go
@@ -21,16 +21,16 @@ func (handler *Handler) handleSlashCommand(ctx context.Context, logger *zap.Logg
 
 	cmtBody := cmt.GetBody()
 	if strings.HasPrefix(cmtBody, "/rerun-workflow") {
-		return handler.rerunWorkflows(ctx, logger, repoCfg.GitHub, repoCfg.RepoOwner, repoCfg.RepoName, cmtBody)
+		return handler.rerunWorkflows(ctx, logger, repoCfg.GitHub, repoCfg.RepoOwner, repoCfg.CIRepoName, cmtBody)
 	}
 	if strings.HasPrefix(cmtBody, "/rerun-failed-job") {
-		return handler.rerunFailedJobs(ctx, logger, repoCfg.GitHub, repoCfg.RepoOwner, repoCfg.RepoName, cmtBody)
+		return handler.rerunFailedJobs(ctx, logger, repoCfg.GitHub, repoCfg.RepoOwner, repoCfg.CIRepoName, cmtBody)
 	}
 	// if strings.HasPrefix(cmtBody, "/rerun-job") {
 	// 	return handler.rerunJobs(ctx, logger, gh, owner, repoName, cmtBody)
 	// }
 	if strings.HasPrefix(cmtBody, "/cancel") {
-		return handler.cancelWorkflows(ctx, logger, repoCfg.GitHub, repoCfg.RepoOwner, repoCfg.RepoName, cmtBody)
+		return handler.cancelWorkflows(ctx, logger, repoCfg.GitHub, repoCfg.RepoOwner, repoCfg.CIRepoName, cmtBody)
 	}
 	return nil, nil //nolint:nilnil
 }
