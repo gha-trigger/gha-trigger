64fa6892181475c3a994a67a4a70c155c35d3ecb
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-09-07 15:24:00 +0900

    feat: validate event type

--- pkg/config/type.go
+++ pkg/config/type.go
@@ -60,7 +60,7 @@ type GitHubAppSecret struct {
 type Event struct {
 	// OR Condition
 	Matches  []*Match
-	Workflow *Workflow
+	Workflow *Workflow `validate:"required"`
 }
 
 type StringMatch struct {
@@ -169,6 +169,7 @@ func (mc *Match) Compile() error {
 }
 
 type EventType struct {
-	Name  string `validate:"required"`
+	// https://docs.github.com/en/actions/using-workflows/events-that-trigger-workflows
+	Name  string `validate:"required,oneof=branch_protection_rule check_run check_suite create delete deployment deployment_status discussion discussion_comment fork gollum issue_comment issues label merge_group milestone page_build project project_card project_column public pull_request pull_request_review pull_request_review_comment pull_request_target push registry_package release repository_dispatch schedule status watch workflow_call workflow_dispatch workflow_run"`
 	Types []string
 }
