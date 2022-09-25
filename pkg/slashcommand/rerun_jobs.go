5aad181afb28fc73d95de3ddc6811e41f898a1cd
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-09-25 10:44:36 +0900

    feat: support rerun-job

--- pkg/slashcommand/rerun_jobs.go
+++ pkg/slashcommand/rerun_jobs.go
@@ -1,53 +1,31 @@
 package slashcommand
 
-// import (
-// 	"context"
-// 	"net/http"
-// 	"strings"
-//
-// 	"github.com/gha-trigger/gha-trigger/pkg/github"
-// 	"github.com/gha-trigger/gha-trigger/pkg/util"
-// 	"go.uber.org/zap"
-// )
-//
-// func rerunJobs(ctx context.Context, logger *zap.Logger, gh *github.Client, owner, repo, cmtBody string) (*Response, error) {
-// 	// /rerun-job <job id> [<job id> ...]
-// 	words := strings.Split(strings.TrimSpace(cmtBody), " ")
-// 	if len(words) < 2 { //nolint:gomnd
-// 		return &Response{
-// 			StatusCode: http.StatusBadRequest,
-// 			Body: map[string]interface{}{
-// 				"error": "job ids are required",
-// 			},
-// 		}, nil
-// 	}
-//
-// 	var gErr error
-// 	resp := &Response{
-// 		StatusCode: http.StatusOK,
-// 		Body: map[string]interface{}{
-// 			"message": "jobs are rerun",
-// 		},
-// 	}
-// 	for _, jobID := range words[1:] {
-// 		runID, err := util.ParseInt64(jobID)
-// 		if err != nil {
-// 			logger.Warn("parse a job id as int64", zap.Error(err))
-// 			if resp.StatusCode == http.StatusOK {
-// 				resp.StatusCode = http.StatusBadRequest
-// 			}
-// 			continue
-// 		}
-// 		if res, err := gh.RerunJob(ctx, owner, repo, runID); err != nil {
-// 			logger.Error("rerun a job", zap.Error(err), zap.Int("status_code", res.StatusCode))
-// 			resp = &Response{
-// 				StatusCode: http.StatusInternalServerError,
-// 				Body: map[string]interface{}{
-// 					"message": "failed to rerun a job",
-// 				},
-// 			}
-// 			continue
-// 		}
-// 	}
-// 	return resp, gErr
-// }
+import (
+	"context"
+
+	"github.com/gha-trigger/gha-trigger/pkg/github"
+	"go.uber.org/zap"
+)
+
+func rerunJobs(ctx context.Context, logger *zap.Logger, gh *github.Client, owner, repo string, words []string) {
+	// /rerun-job <job id> [<job id> ...]
+	if len(words) == 0 { //nolint:gomnd
+		// TODO send notification to issue or pr
+		logger.Warn("job id is required for /rerun-job")
+		return
+	}
+
+	ids, err := parseIDs(words)
+	if err != nil {
+		logger.Warn("parse a job id as int64", zap.Error(err))
+		return
+	}
+
+	for _, jobID := range ids {
+		logger := logger.With(zap.Int64("job_id", jobID))
+		if res, err := gh.RerunJob(ctx, owner, repo, jobID); err != nil {
+			// TODO send a notification to pr or issue
+			logger.Error("rerun a job", zap.Error(err), zap.Int("status_code", res.StatusCode))
+		}
+	}
+}
